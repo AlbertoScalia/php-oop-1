87a5c1376926709c231e1e01f75b71d64a74a2f4
AlbertoScalia <albscalia@gmail.com>
2023-06-20 19:13:02 +0200

    Feat: Add movie table on page

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php 
+
+include_once __DIR__ . '/Models/Movie.php';
+include_once __DIR__ . '/Models/Genre.php';
+
+$genres = [
+    "action" => new Genre('Action'),
+    "adventure" => new Genre('Adventure'),
+    "anime" => new Genre('Anime'),
+    "comedy" => new Genre('Comedy'),
+    "dramatic" => new Genre('Dramatic'),
+    "horror" => new Genre('Horror'),
+    "mystery" => new Genre('Mystery'),
+    "sciFi" => new Genre('Science Fiction'),
+    "thriller" => new Genre('Thriller')
+];
+
+$movies = [
+    new Movie('Spirited Away', 'Japanese', $genres["action"]),
+    new Movie('The Ninth Gate', 'English', $genres["thriller"]),
+    new Movie('Twin Peaks', 'English', $genres["mystery"]),
+    new Movie('Interstellar', 'English',$genres["sciFi"]),
+    new Movie('Blade Runner', 'English', $genres["sciFi"]),
+    new Movie('Old Boy', 'Korean', $genres["thriller"])
+];
--- index.php
+++ index.php
@@ -1,56 +1,54 @@
-<?php 
+<?php
 
-class Genre {
-    public $name;
-    public function __construct(String $name) {
-        $this->name = $name;
-    }
-};
+include_once __DIR__ . '/db.php';
 
-class Movie {
-    public $title;
-    public $language;
-    public $genre;
-    public $isRatedR = false;
-    public $type = "movie";
-    function __construct(String $title, String $language, Genre $genre) {
-        $this->title = $title;
-        $this->language = $language;
-        $this->genre = $genre;
-    }
-    function isRatedR($minAge) {
-        if ($minAge >= 18) {
-            $this->isRatedR = true;
-        } 
-    }
-};
-
-$animeGenre = new Genre('Anime');
-$thrillerGenre = new Genre('Thriller');
-
-
-$spiritedAway = new Movie('Spirited Away', 'Japanese', $animeGenre);
-
-$theNinthGate = new Movie('The Ninth Gate', 'English', $thrillerGenre);
-$theNinthGate->isRatedR(20);
-
-var_dump($spiritedAway, $theNinthGate);
+var_dump($movies);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
-    <div class="container">
-        <h1>My OOP Movies</h1>
+    <div id="app">
+        <div class="container">
+            <h1 class="py-4">My OOP Movies</h1>
 
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Language</th>
+                        <th scope="col">Genres</th>
+                        <th scope="col">Rated R</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($movies as $movie) : ?>
+                    <tr>
+                        <td><?= $movie->title?></td>
+                        <td><?= $movie->language?></td>
+                        <td><?= $movie->genre->name?></td>
+                        <?php if ($movie->isRatedR === true) :?>
+                            <td>Yes</td>
+                        <?php else : ?>
+                        <td>No</td>
+                        <?php endif ?>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
 
+        </div>
     </div>
 </body>
+
 </html>
